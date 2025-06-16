3baf44cf97d200a64106e84acb1e68b21aac29dc
DESKTOP-GR883KV\user <`12123>
2025-06-16 15:22:42 +0900

    ch3 complete

---
+++ ch3-spring-db1/hands-on/src/main/java/com/example/handson/dto/PostUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.example.handson.dto;
+
+import com.example.handson.domain.Post;
+import lombok.Data;
+
+@Data
+public class PostUpdateRequest {
+    private String title;
+    private String body;
+
+    public Post toDomain() {
+        Post post = new Post();
+        post.setTitle(this.title);
+        post.setBody(this.body);
+        return post;
+    }
+}
--- ch3-spring-db1/hands-on/src/main/java/com/example/handson/mapper/PostMapper.java
+++ ch3-spring-db1/hands-on/src/main/java/com/example/handson/mapper/PostMapper.java
@@ -1,7 +1,7 @@
-package com.example.ch3examples.mapper;
+package com.example.handson.mapper;
 
-import com.example.ch3examples.domain.Post;
-import com.example.ch3examples.dto.PostSearchRequest;
+import com.example.handson.domain.Post;
+import com.example.handson.dto.PostSearchRequest;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,6 +9,7 @@ import java.util.List;
 @Mapper
 public interface PostMapper {
     void save(Post post);
+//    List<Post> findAll();
     List<Post> findAll(PostSearchRequest search);
     int count(PostSearchRequest search); // 전체 개수
     Post findById(Long id);
